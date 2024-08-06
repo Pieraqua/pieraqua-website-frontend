47bad3100d93a357fadea56fa6eac895e92f98e6
pieraqua <pauloffxii@hotmail.com>
2024-08-06 19:21:54 -0300

    Inicio do desenvolvimento do frontend de blogs.

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 import {ref} from 'vue';
 import {api} from './environment.js';
 const isLoading  = ref<boolean>(true);
-const msg  = ref<string>("");
+const blogs  = ref<Array<string>>([]);
 
 async function getData()
 {
   try {
-    const response = (await fetch(api));
-    const text = await response.text();
-    msg.value = text;
+    const response = (await fetch(api + '/blogs'));
+    const text : string = await response.text();
+    blogs.value = JSON.parse(text);
   } catch(e) {
     console.error('Failed to retrieve data: ', e);
   }
@@ -24,42 +24,20 @@ getData()
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <p v-if="!isLoading">{{msg}}</p>
+    <h1>Bem vindo ao meu site pessoal!</h1>
+    <div>
     </div>
   </header>
 
   <main>
+    <div v-if="!isLoading">
+      <div v-for="item in blogs">
+        <h3>{{ item }}</h3>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
